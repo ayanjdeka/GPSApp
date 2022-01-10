64186446cd0aef075cc0b908a5be1333286b568d
Ayan Deka <50529339+ayanjdeka@users.noreply.github.com>
2022-01-09 21:50:54 -0500

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/github/ayan/gpsprojectnexis/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/github/ayan/gpsprojectnexis/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.rafid.gpsprojectnexis", appContext.getPackageName());
+        assertEquals("com.github.ayan.gpsprojectnexis", appContext.getPackageName());
     }
 }
